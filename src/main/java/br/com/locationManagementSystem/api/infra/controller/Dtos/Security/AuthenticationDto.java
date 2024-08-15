41dfdcc7a25d34a710674bcfe34a54d26b8ae03b
datdudu <c.eduardocarvalhoc@gmail.com>
2024-08-15 11:44:45 -0300

    Fixing some error on the application runtime

--- src/main/java/br/com/locationManagementSystem/api/infra/controller/Dtos/Security/AuthenticationDto.java
+++
@@ -1,4 +0,0 @@
-package br.com.locationManagementSystem.api.infra.controller.Dtos.Security;
-
-public record AuthenticationDto(String login, String password) {
-}
--- src/main/java/br/com/locationManagementSystem/api/infra/controller/Dtos/User/UserResetPasswordDto.java
+++
@@ -1,4 +0,0 @@
-package br.com.locationManagementSystem.api.infra.controller.Dtos.User;
-
-public record UserResetPasswordDto(String email, String password) {
-}
